951db74aab2df732f6cedc741b94d96725831b9a
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-11 12:26:12 +0300

    get bids of item definition

--- webapi/Repository/IBidRepository.cs
+++ webapi/Repository/IBidRepository.cs
@@ -4,6 +4,8 @@ namespace webapi.Repository
 {
     public interface IBidRepository
     {
-        public Task<Bid> Create(long itemId);
+        public Task<Bid> Create(long itemId, CancellationToken cancel = default);
+
+        public Task<IEnumerable<Bid>> GetItemBids(long itemId, CancellationToken cancel = default);
     }
 }
--- webapi/Services/IBidService.cs
+++ webapi/Services/IBidService.cs
@@ -4,6 +4,8 @@ namespace webapi.Services
 {
     public interface IBidService
     {
-        public Task<Bid> Create(long itemId);
+        public Task<Bid> Create(long itemId, CancellationToken cancel = default);
+
+        public Task<IEnumerable<Bid>> GetItemBids(long itemId, CancellationToken cancel = default);
     }
 }
