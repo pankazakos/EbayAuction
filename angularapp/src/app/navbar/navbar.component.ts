0c19c57dc949492cee0cdf35fcf999250af0c5da
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-16 15:15:17 +0300

    align items login button

--- angularapp/src/app/navbar/navbar.component.ts
+++ angularapp/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
@@ -7,7 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule]
+  imports: [MatToolbarModule, MatGridListModule, MatButtonModule]
 })
 export class NavBarComponent {
 
