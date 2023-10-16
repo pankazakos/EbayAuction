65349e2df9093bdd3fa5785851dac78f482afc1a
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-16 15:35:55 +0300

    add login-modal component

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginModalComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule, BrowserAnimationsModule, NavBarComponent
