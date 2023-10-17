c3ba6fff60a153724a4357363a7f50187990bcb6
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-17 11:39:49 +0300

    add sign in and sign up components

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { NavBarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModalComponent } from './login-modal/login-modal.component';
 import { LoginTabsComponent } from './login-modal/login-tabs/login-tabs.component';
+import { SignInComponent } from './login-modal/login-tabs/sign-in/sign-in.component';
+import { SignUpComponent } from './login-modal/login-tabs/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignInComponent, SignUpComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
