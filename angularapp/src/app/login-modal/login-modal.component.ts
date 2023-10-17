4c2cf2e806289f15826997663791e794f11aa216
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-17 11:31:49 +0300

    close icon button

--- angularapp/src/app/login-modal/login-modal.component.ts
+++ angularapp/src/app/login-modal/login-modal.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { LoginTabsComponent } from './login-tabs/login-tabs.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.css'],
   standalone: true,
-  imports: [MatDialogModule, LoginTabsComponent],
+  imports: [
+    MatDialogModule,
+    LoginTabsComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class LoginModalComponent {}
