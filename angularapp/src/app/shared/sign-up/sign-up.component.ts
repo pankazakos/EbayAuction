c825956fb9e27359f8a3985da1c726adc77b89cc
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-20 17:16:04 +0300

    sign up request

--- angularapp/src/app/shared/sign-up/sign-up.component.ts
+++ angularapp/src/app/shared/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { CardWrapperCssProps } from '../card/card.component';
 
 @Component({
@@ -13,4 +15,24 @@ export class SignUpComponent {
     height: '100%',
     width: '100%',
   };
+
+  @ViewChild('loginForm') registerForm!: NgForm;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const userInfo = this.registerForm.value;
+    console.log(userInfo);
+
+    if (this.registerForm.valid) {
+      this.http.post('https://localhost:7068/api/User', userInfo).subscribe({
+        next: (response) => {
+          console.log('sign up successful');
+        },
+        error: (error) => {
+          console.error('sign up failed', error);
+        },
+      });
+    }
+  }
 }
