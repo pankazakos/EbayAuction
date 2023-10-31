43da34961a1952edb8bf3c8723ffdeefedf46960
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-31 13:19:08 +0200

    item service logging

--- webapi/Services/ItemService.cs
+++ webapi/Services/ItemService.cs
@@ -9,30 +9,39 @@ namespace webapi.Services
     {
         private readonly IItemRepository _itemRepository;
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<ItemService> _logger;
 
-        public ItemService(IItemRepository itemRepository, IUserRepository userRepository)
+        public ItemService(IItemRepository itemRepository, IUserRepository userRepository, ILogger<ItemService> logger)
         {
             _itemRepository = itemRepository;
             _userRepository = userRepository;
+            _logger = logger;
         }
 
         public async Task<Item> Create(AddItemRequest item, string username, IFormFile? postedFile, CancellationToken cancel)
         {
             if (item is null)
             {
-                throw new ArgumentException("Invalid data.");
+                const string message = "Invalid data to create item";
+                _logger.LogWarning(message);
+                throw new ArgumentException(message);
             }
 
             var user = await _userRepository.GetByUsername(username, cancel);
 
             if (user is null)
             {
-                throw new InvalidOperationException("User not found");
+                _logger.LogWarning("User {username} not found", username);
+                throw new InvalidOperationException($"User {username} not found");
             }
 
             if (postedFile is null)
             {
-                return await _itemRepository.Create(item, user, cancel: cancel);
+                var createdItem = await _itemRepository.Create(item, user, cancel: cancel);
+
+                _logger.LogInformation("Item {itemId} created", createdItem.ItemId);
+
+                return createdItem;
             }
 
             var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
@@ -41,29 +50,60 @@ namespace webapi.Services
 
             if (!allowedExtensions.Contains(fileExtension))
             {
-                throw new ArgumentException("Invalid file type. Only .jpg, .jpeg, and .png files are allowed.");
+                const string message = "Invalid file type. Only .jpg, .jpeg, and .png files are allowed";
+                _logger.LogWarning(message);
+                throw new ArgumentException(message);
             }
 
             var fileName = Guid.NewGuid() + fileExtension;
 
-            return await _itemRepository.Create(item, user, postedFile, fileName, cancel);
+            var createdItemWithImage = await _itemRepository.Create(item, user, postedFile, fileName, cancel);
+
+            _logger.LogInformation("Item {itemId} created", createdItemWithImage.ItemId);
+
+            return createdItemWithImage;
         }
 
         public async Task<IEnumerable<Item>> ListAll(CancellationToken cancel = default)
         {
-            return await _itemRepository.ListAll(cancel);
+            var items = await _itemRepository.ListAll(cancel);
+
+            _logger.LogInformation("All items retrieved");
+
+            return items;
         }
 
 
         public async Task<Item?> GetById(long id, CancellationToken cancel)
         {
-            return await _itemRepository.GetById(id, cancel);
+            var item = await _itemRepository.GetById(id, cancel);
+
+            if (item is null)
+            {
+                _logger.LogWarning("Item {itemId} not found", id);
+                throw new InvalidOperationException($"Item {id} not found");
+            }
+
+            _logger.LogInformation("Item {itemId} retrieved", item.ItemId);
+
+            return item;
         }
 
 
         public async Task<List<Item>> GetItemsOfUserBasedOnStatus(int userId, bool active, CancellationToken cancel)
         {
-            return await _itemRepository.GetItemsOfUserBasedOnStatus(userId, active, cancel);
+            var userItems = await _itemRepository.GetItemsOfUserBasedOnStatus(userId, active, cancel);
+
+            if (userItems.Any())
+            {
+                return userItems;
+            }
+
+            var status = active ? "active" : "inactive";
+
+            _logger.LogWarning("No {status} items found for user {userId}", status, userId);
+
+            throw new InvalidOperationException($"No {status} items found for user {userId}");
         }
 
         public async Task<Item> Activate(long id, PublishItemRequest input, CancellationToken cancel = default)
@@ -74,7 +114,9 @@ namespace webapi.Services
 
             if (!DateTime.TryParseExact(dtInput, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var dtExpiration))
             {
-                throw new ArgumentException("DateTime format is not correct. Correct format is: \"yyyy-MM-dd HH:mm\" ");
+                const string message = "DateTime format is not correct. Correct format is: \"yyyy-MM-dd HH:mm\" ";
+                _logger.LogWarning(message);
+                throw new ArgumentException(message);
             }
 
             var now = DateTime.Now.ToString(format);
@@ -83,10 +125,16 @@ namespace webapi.Services
 
             if (dtExpiration <= dtNow)
             {
-                throw new ArgumentException("Ends datetime cannot be set to a datetime earlier than the current datetime.");
+                const string message = "Ends datetime cannot be set to a datetime earlier than the current datetime";
+                _logger.LogWarning(message);
+                throw new ArgumentException(message);
             }
 
-            return await _itemRepository.Activate(id, input.Expiration, cancel);
+            var item = await _itemRepository.Activate(id, input.Expiration, cancel);
+
+            _logger.LogInformation("Activated item {itemId}", item.ItemId);
+
+            return item;
         }
 
         public async Task Delete(long id, CancellationToken cancel = default)
@@ -95,10 +143,13 @@ namespace webapi.Services
 
             if (item is null)
             {
-                throw new InvalidOperationException("item not found");
+                _logger.LogWarning("Item {itemId} not found", id);
+                throw new InvalidOperationException($"item {id} not found");
             }
 
             await _itemRepository.Delete(item, cancel);
+
+            _logger.LogInformation("Deleted item {itemId}", id);
         }
     }
 }
