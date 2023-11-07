924793bdfde84ec6156c839bd8ca54e1cdffb591
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-11-07 12:43:05 +0200

    create item test

---
+++ tests/Api.IntegrationTests/ItemController/CreateItemTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http.Headers;
+using contracts.Requests.Item;
+using Newtonsoft.Json;
+using System.Text;
+using contracts.Responses.Item;
+using FluentAssertions;
+
+namespace Api.IntegrationTests.ItemController
+{
+    public class CreateItemTest
+    {
+        private readonly HttpClient _client;
+        private readonly string _simpleUserJwt;
+
+        public CreateItemTest()
+        {
+            _client = new HttpClient();
+            _simpleUserJwt = Utils.LoginOrCreateSimpleUser(_client).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public async Task CreateItem_ReturnsItem_WhenInputIsCorrectWithoutImage()
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _simpleUserJwt);
+
+            var itemData = new AddItemRequest
+            {
+                Name = "test item",
+                FirstBid = 50,
+                CategoryIds = new List<int> { 1, 2 },
+                Description = "test item description"
+            };
+
+            var addItemBody = new StringContent(JsonConvert.SerializeObject(itemData), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"{Utils.BaseUrl}item", addItemBody);
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var createdItem = JsonConvert.DeserializeObject<AddItemResponse>(responseString);
+
+            createdItem.Should().NotBeNull();
+
+            createdItem!.Name.Should().Be(itemData.Name);
+
+            await _client.DeleteAsync($"{Utils.BaseUrl}item/{createdItem.ItemId}");
+        }
+
+
+    }
+}
--- tests/Api.IntegrationTests/Utils.cs
+++ tests/Api.IntegrationTests/Utils.cs
@@ -1,4 +1,5 @@
-﻿using contracts.Requests.User;
+﻿using System.Net;
+using contracts.Requests.User;
 using Newtonsoft.Json;
 using System.Text;
 using contracts.Responses;
@@ -33,21 +34,59 @@ namespace Api.IntegrationTests
             response.EnsureSuccessStatusCode();
 
             var responseString = await response.Content.ReadAsStringAsync();
-            var responseBody = JsonConvert.DeserializeObject<LoginUserResponse>(responseString);
 
-            if (responseBody is null)
-            {
-                throw new InvalidOperationException("Couldn't get jwt of admin");
-            }
-
+            var responseBody = JsonConvert.DeserializeObject<LoginUserResponse>(responseString) ?? 
+                               throw new InvalidOperationException("Couldn't get jwt of admin");
+            
             AdminToken = responseBody.AccessToken;
 
             return responseBody.AccessToken;
         }
 
-        //public static async Task<BasicUserResponse> GetOrCreateUser()
-        //{
+        public static async Task<string> LoginOrCreateSimpleUser(HttpClient client)
+        {
+            var userCredentials = new LoginUserRequest
+            {
+                Username = "simpleUser",
+                Password = "123"
+            };
 
-        //}
+            var loginBody = new StringContent(JsonConvert.SerializeObject(userCredentials), Encoding.UTF8, "application/json");
+
+            var loginResponse = await client.PostAsync($"{BaseUrl}user/login", loginBody);
+
+            // Create user if not found
+            if (loginResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                var userInfo = new RegisterUserRequest
+                {
+                    Username = "simpleUser",
+                    Password = "123",
+                    FirstName = "firstName",
+                    LastName = "lastName",
+                    Email = "simpleUser@test.com",
+                    Country = "test country",
+                    Location = "test location",
+                };
+
+                var registerBody = new StringContent(JsonConvert.SerializeObject(userInfo), Encoding.UTF8, "application/json");
+
+                var registerResponse = await client.PostAsync($"{BaseUrl}user/register", registerBody);
+
+                registerResponse.EnsureSuccessStatusCode();
+            }
+
+            loginResponse = await client.PostAsync($"{BaseUrl}user/login", loginBody);
+
+            var responseString = await loginResponse.Content.ReadAsStringAsync();
+            var responseBody = JsonConvert.DeserializeObject<LoginUserResponse>(responseString);
+
+            if (responseBody != null)
+            {
+                return responseBody.AccessToken;
+            }
+
+            throw new InvalidOperationException("Error while getting the jwt of simple user.");
+        }
     }
 }
