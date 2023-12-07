7d3009982404ad0395dec58d6fef040ae1f487ab
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-12-07 17:02:48 +0200

    bid button

--- angularapp/src/app/material-ui/material-ui.module.ts
+++ angularapp/src/app/material-ui/material-ui.module.ts
@@ -18,6 +18,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   imports: [
@@ -40,6 +41,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatAutocompleteModule,
     MatSnackBarModule,
     MatTooltipModule,
+    MatExpansionModule,
   ],
   exports: [
     MatIconModule,
@@ -61,6 +63,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatAutocompleteModule,
     MatSnackBarModule,
     MatTooltipModule,
+    MatExpansionModule,
   ],
 })
 export class MaterialUiModule {}
