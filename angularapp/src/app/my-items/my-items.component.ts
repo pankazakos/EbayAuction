5d5639c5e8f91ff26db67f42b98f8004b64f88f7
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-12-15 12:40:16 +0200

    use guard to reduce loading times of items on tab change

--- angularapp/src/app/my-items/my-items.component.ts
+++ angularapp/src/app/my-items/my-items.component.ts
@@ -18,6 +18,10 @@ export class MyItemsComponent {
   publishedItems: PublishedItemResponse[];
   itemsWithBids: BasicItemResponse[];
 
+  updatedInactive: boolean = false;
+  updatedPublished: boolean = false;
+  updatedBidden: boolean = false;
+
   constructor(private http: HttpClient) {
     this.inactiveItems = [];
     this.publishedItems = [];
@@ -26,6 +30,10 @@ export class MyItemsComponent {
       'Authorization',
       `Bearer ${localStorage.getItem('accessToken')}`
     );
+  }
+
+  ngOnInit(): void {
+    this.setInactiveItems();
   }
 
   onTabChanged(event: MatTabChangeEvent): void {
@@ -47,39 +55,50 @@ export class MyItemsComponent {
   }
 
   setInactiveItems(): void {
+    if (this.updatedInactive) return;
+
     this.http.get(ItemEndpoints.inactive, { headers: this.headers }).subscribe({
-      next: (response: BasicItemResponse[]) => {
+      next: (response: BasicItemResponse[] | any) => {
+        console.log(response);
+
         this.inactiveItems = response;
+        this.updatedInactive = true;
       },
       error: (error: any) => {
         console.log(error);
       },
-    } as Partial<any>);
+    });
   }
 
   setPublishedItems(): void {
+    if (this.updatedPublished) return;
+
     this.http.get(ItemEndpoints.active, { headers: this.headers }).subscribe({
-      next: (response: PublishedItemResponse[]) => {
+      next: (response: PublishedItemResponse[] | any) => {
+        console.log(response);
+
         this.publishedItems = response;
+        this.updatedPublished = true;
       },
       error: (error: any) => console.error(error),
-    } as Partial<any>);
+    });
   }
 
   setItemsWithBids(): void {
+    if (this.updatedBidden) return;
+
     this.http.get(ItemEndpoints.bidden, { headers: this.headers }).subscribe({
-      next: (response: BasicItemResponse[]) => {
+      next: (response: BasicItemResponse[] | any) => {
+        console.log(response);
+
         this.itemsWithBids = response;
+        this.updatedBidden = true;
       },
       error: (error: any) => console.error(error),
-    } as Partial<any>);
+    });
   }
 
   addItem(): void {
-    // this.http.post(
-    //   ItemEndpoints.create,
-    //   // { body: {} },
-    //   { headers: this.headers }
-    // );
+    console.log('add item');
   }
 }
